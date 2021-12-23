dfc6c00d86734cfeb54d2ca5aa4243403a98aac0
William Brawner <me@wbrawner.com>
2021-12-23 06:24:46 -0700

    Rename package to TwigsCore for importing into other apps

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TwigsCore",
-            targets: ["Twigs"]),
+            targets: ["TwigsCore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Twigs",
+            name: "TwigsCore",
             dependencies: []),
         .testTarget(
-            name: "TwigsTests",
-            dependencies: ["Twigs"]),
+            name: "TwigsCoreTests",
+            dependencies: ["TwigsCore"]),
     ]
 )
