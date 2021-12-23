2859f22f5b1475dbcb250ac38ee6038f04c8bee7
William Brawner <me@wbrawner.com>
2021-12-23 06:16:11 -0700

    Rename package to TwigsCore for importing into other apps

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Twigs",
+            name: "TwigsCore",
             targets: ["Twigs"]),
     ],
     dependencies: [
